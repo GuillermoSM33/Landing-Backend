61e1a05d8ede48338d1cac58ca0d4ce34eb5c7f8
eduardo87DR <spixerspi@gmail.com>
2025-06-22 19:40:37 -0500

    Enabled CORS with Frontend

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,18 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  // Configuración de CORS
+  app.enableCors({
+    origin: 'http://localhost:5173', 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  });
+  
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
+  
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
