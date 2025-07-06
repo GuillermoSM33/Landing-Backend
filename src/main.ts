816ec709a0564666b5b1c602b6f0ade8356ab7ab
GuillermoSM33 <guillermo.jesus.garcia.canul@gmail.com>
2025-07-06 08:42:40 -0600

    UPDATE: MAIN.TS ADD LOGISTIC FOR STATIC FRONT

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as express from 'express';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
   // Configuración de CORS
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://localhost:5174'], 
+    origin: ['http://localhost:5173', 'http://localhost:5174'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
     allowedHeaders: 'Content-Type, Accept, Authorization',
   });
-  
+
+  // Middleware para servir archivos estáticos del frontend
+  app.use(express.static(join(__dirname, '..', 'public')));
+
+  app.use((req, res, next) => {
+    res.sendFile(join(__dirname, '..', 'public', 'index.html'));
+  });
+
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
-  
+
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
